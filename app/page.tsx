692a5045d57e7cd63afa6f4ad3f942597171d461
robsonfdemelo <robson.ferreiramelo@yahoo.com.br>
2025-08-04 14:40:44 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import { SearchIcon } from "lucide-react"
 import Header from "./_components/ui/header"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return (
@@ -12,6 +15,7 @@ const Home = () => {
         <h2 className="text-xl font-bold">Olá, Robinho!</h2>
         <p>Sábado, 02 de Agosto de 2025</p>
 
+        {/* Busca */}
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Faça Sua Busca" />
           <Button>
@@ -19,6 +23,7 @@ const Home = () => {
           </Button>
         </div>
 
+        {/* Banner */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             src="/banner-01.png"
@@ -28,6 +33,34 @@ const Home = () => {
             priority
           />
         </div>
+
+        {/* Agendamento */}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* Esquerda */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6 overflow-hidden">
+                  <AvatarImage
+                    src="/avatar.png"
+                    alt="Foto da barbearia"
+                    className="object-cover"
+                  />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+
+            {/* Direita */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
